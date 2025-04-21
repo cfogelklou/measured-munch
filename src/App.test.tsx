97958ba21c91e6e55e8a4e39c4f4547756ce4a1c
Chris Fogelklou <chris.fogelklou@gmail.com>
2025-04-21 07:38:20 +0200

    wip

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 import { LSProvider } from './context';
 
@@ -12,4 +12,31 @@ describe('App Component', () => {
     );
     expect(getByText('Measured Munch')).toBeInTheDocument();
   });
-});
+
+  it('adds a new fasting record and verifies it is added to the history', async () => {
+    const { getByText } = render(
+      <LSProvider>
+        <App />
+      </LSProvider>,
+    );
+
+    // Start a fast
+    const startButton = getByText('Start Fast');
+    fireEvent.click(startButton);
+
+    // Delay for 5 seconds to simulate fasting
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    // Stop the fast
+    const stopButton = getByText('Stop Fast');
+    fireEvent.click(stopButton);
+
+    // Switch to the "Stats" tab
+    const statsTab = getByText('Stats');
+    fireEvent.click(statsTab);
+
+    // Verify the fasting record is added
+    expect(getByText('Successful Fasts:')).toBeInTheDocument();
+    //expect(getByText('1')).toBeInTheDocument();
+  });
+}, 10000); // Set a timeout for the test to allow for async operations
